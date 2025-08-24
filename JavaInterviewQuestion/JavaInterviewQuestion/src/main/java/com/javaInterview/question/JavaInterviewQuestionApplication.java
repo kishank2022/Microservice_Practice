de890ab680ef84957558f9d245b1ef839faefcf6
Kishan Kushwaha <kishankushwaha076@gmail.com>
2025-08-24 19:57:51 +0530

    test

---
+++ JavaInterviewQuestion/JavaInterviewQuestion/src/main/java/com/javaInterview/question/JavaInterviewQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.javaInterview.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInterviewQuestionApplication.class, args);
+	}
+
+}
