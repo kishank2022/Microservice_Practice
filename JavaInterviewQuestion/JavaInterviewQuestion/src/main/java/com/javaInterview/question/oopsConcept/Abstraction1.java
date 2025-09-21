4ef79079239303253285a7c261e24b4b2c9d1289
Kishan Kushwaha <kishankushwaha076@gmail.com>
2025-09-21 16:26:40 +0530

    oop's

---
+++ JavaInterviewQuestion/JavaInterviewQuestion/src/main/java/com/javaInterview/question/oopsConcept/Abstraction1.java
@@ -0,0 +1,17 @@
+package com.javaInterview.question.oopsConcept;
+
+
+/*
+	Abstraction - The process of hiding implementation details and showing only the essential features of an object.
+	Focuses on what an object does, not how it does it.
+	Supports loose coupling → promotes maintainable code.
+	Hides implementation details → Users only see the method signature.
+	
+	Abstract classes can have constructor, static methods
+	Interface cannot have constructor.
+	
+*/
+
+public class Abstraction1 {
+
+}
