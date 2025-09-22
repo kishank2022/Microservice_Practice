ab2d8f2dcfe710dd2cf6aa111746d266f38b5703
kishank2022 <kishankushwaha076@gmail.com>
2025-09-22 11:14:19 +0530

    microservice questions

---
+++ JavaInterviewQuestion/JavaInterviewQuestion/src/main/java/com/javaInterview/question/microservice/LoadBalancing.java
@@ -0,0 +1,6 @@
+package com.javaInterview.question.microservice;
+
+// Load Balancing - Load balancing is the process of distributing incoming network traffic (requests) across multiple servers (or service instances).
+public class LoadBalancing {
+
+}
--- JavaInterviewQuestion/JavaInterviewQuestion/src/main/java/com/javaInterview/question/microservice/MicroserviceQuestion.java
+++ JavaInterviewQuestion/JavaInterviewQuestion/src/main/java/com/javaInterview/question/microservice/MicroserviceQuestion.java
@@ -2,5 +2,5 @@ package com.javaInterview.question.microservice;
 
 public class MicroserviceQuestion {
 
-	// hello
+	
 }
