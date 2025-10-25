40a80e025410ff05c1f4b1d9760c8cc16905d304
Kishan Kushwaha <kishankushwaha076@gmail.com>
2025-10-25 15:31:12 +0530

    kafka-schema-registry

---
+++ schedular_spring_boot/schedular_spring_boot/src/test/java/com/code/with/durgesh/schedular/schedular_spring_boot/SchedularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.with.durgesh.schedular.schedular_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
