d0646cb89070e9a434a6c337fb2569718ad1a0e6
Kishan Kushwaha <kishankushwaha076@gmail.com>
2026-03-03 17:49:41 +0530

    commit new code

---
+++ JavaSpringBootWithMysql/JavaSpringBootWithMysql/src/main/java/com/java/with/mysql/JavaSpringBootWithMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.java.with.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootWithMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootWithMysqlApplication.class, args);
+	}
+
+}
---
+++ JavaSpringBootWithMysql/JavaSpringBootWithMysql/src/test/java/com/java/with/mysql/JavaSpringBootWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.with.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
